fc99be4213cbb2a0412a2599272e72f388454314
Morgan Lombard <morgan.lombard@protonmail.com>
2020-11-28 18:18:37 +0100

    #7 implémentation des DTO

---
+++ src/main/java/com/progparcomposant/reseausocial/dto/UserDTO.java
@@ -0,0 +1,104 @@
+package com.progparcomposant.reseausocial.dto;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+public class UserDTO {
+
+    private Long idUser;
+    private String firstName;
+    private String lastName;
+    private Date birthdate;
+    private String email;
+    private String phoneNumber;
+    private String city;
+    private Timestamp signInDate;
+    private String password;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(Long idUser, String firstName, String lastName, Date birthdate, String email, String phoneNumber, String city, Timestamp signInDate, String password) {
+        this.idUser = idUser;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthdate = birthdate;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.city = city;
+        this.signInDate = signInDate;
+        this.password = password;
+    }
+
+    public Long getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(Long idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Timestamp getSignInDate() {
+        return signInDate;
+    }
+
+    public void setSignInDate(Timestamp signInDate) {
+        this.signInDate = signInDate;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
