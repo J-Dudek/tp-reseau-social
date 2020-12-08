950e5cafacd366504bda4559a023d982beda6526
Pierre <pierre.darcas@lacatholille.fr>
2020-12-08 19:16:42 +0100

    add findPostByUser

--- src/main/java/com/progparcomposant/reseausocial/controllers/PostController.java
+++ src/main/java/com/progparcomposant/reseausocial/controllers/PostController.java
@@ -2,12 +2,15 @@ package com.progparcomposant.reseausocial.controllers;
 
 import com.progparcomposant.reseausocial.converters.PostConverter;
 import com.progparcomposant.reseausocial.model.Post;
+import com.progparcomposant.reseausocial.model.User;
 import com.progparcomposant.reseausocial.repositories.PostRepository;
+import com.progparcomposant.reseausocial.repositories.UserRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @RestController
@@ -15,10 +18,12 @@ import java.util.Optional;
 public class PostController {
 
     private final PostRepository postRepository;
+    private final UserRepository userRepository;
     private final PostConverter postConverter;
 
-    public PostController(PostRepository postRepository, PostConverter postConverter) {
+    public PostController(PostRepository postRepository, UserRepository userRepository, PostConverter postConverter) {
         this.postRepository = postRepository;
+        this.userRepository = userRepository;
         this.postConverter = postConverter;
     }
 
@@ -28,5 +33,13 @@ public class PostController {
     @GetMapping(path="/{postId}")
     public Optional<Post> findPost(@PathVariable("postId") Long postId){ return this.postRepository.findById(postId);}
 
+    @GetMapping(path = "/user/{userId}")
+    public Iterable<Post> findPostsByUser(@PathVariable("userId") Long userId){
+/*        if(!userRepository.existsById(userId)){
+            throw new NoSuchElementException("UserId inexistant")
+        }*/
+        return this.postRepository.findAllByUserId(userId);
+    }
 
+    
 }
--- src/main/java/com/progparcomposant/reseausocial/repositories/PostRepository.java
+++ src/main/java/com/progparcomposant/reseausocial/repositories/PostRepository.java
@@ -1,7 +1,13 @@
 package com.progparcomposant.reseausocial.repositories;
 
 import com.progparcomposant.reseausocial.model.Post;
+import com.progparcomposant.reseausocial.model.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PostRepository extends CrudRepository<Post, Long> {
+    Iterable<Post> findAllByUserId(Long userId);
+    Iterable<Post> findAllByIdIn(List<Long> listIds);
 }
