7a55a6ec2c39e03b5f3b27ee15e3f52fce45cc88
Pierre <pierre.darcas@lacatholille.fr>
2020-12-09 22:28:51 +0100

    end of branch

--- src/main/java/com/progparcomposant/reseausocial/controllers/PostController.java
+++ src/main/java/com/progparcomposant/reseausocial/controllers/PostController.java
@@ -26,13 +26,24 @@ public class PostController {
         this.postConverter = postConverter;
     }
 
+    /**
+     *
+     * @return Iterable<PostDTO>
+     */
     @GetMapping
     public Iterable<PostDTO> findPosts(){ return this.postConverter.entityToDto((List<Post>) this.postRepository.findAll()); }
 
+    /**
+     *
+     * @param postId
+     * @return PostDTO
+     * @throws NoSuchElementException
+     */
     @GetMapping(path="/{postId}")
-    public PostDTO findPost(@PathVariable("postId") Long postId) throws NoSuchElementException{
+    public PostDTO findPostById(@PathVariable("postId") Long postId) throws NoSuchElementException{
 
         Optional<Post> post = this.postRepository.findById(postId);
+        //Check if @param postId is in Database
         if(post.isPresent()){
             return this.postConverter.entityToDto(post.get());
         }else{
@@ -40,20 +51,80 @@ public class PostController {
         }
     }
 
+    /**
+     *
+     * @param userId
+     * @return
+     */
     @GetMapping(path = "/user/{userId}")
     public Iterable<PostDTO> findPostsByUser(@PathVariable("userId") Long userId){
-/*        if(!userRepository.existsById(userId)){
-            throw new NoSuchElementException("UserId inexistant")
-        }*/
+        if(!userRepository.existsById(userId)){
+            throw new NoSuchElementException("UserId inexistant");
+        }
         return this.postConverter.entityToDto((List<Post>) this.postRepository.findAllByUserId(userId));
     }
 
+    /**
+     *
+     * @param listIds
+     * @return Iterable<PostDTO>
+     */
     @GetMapping(path="/list/{listIds}")
     public Iterable<PostDTO> findListPostsByIds(@PathVariable("listIds") List<Long> listIds){
         return this.postConverter.entityToDto((List<Post>) this.postRepository.findAllByIdIn(listIds));}
 
+    /**
+     *
+      * @param newPostDto
+     * @return PostDTO
+     */
     @PostMapping(path="/")
     public PostDTO createPost( @RequestBody PostDTO newPostDto){
         return  postConverter.entityToDto(postRepository.save(this.postConverter.dtoToEntity(newPostDto)));
+    }
+
+    /**
+     *
+     * @param postId
+     * @param newPostDto
+     * @return PostDTO
+     */
+    @PutMapping(path="/{postId}")
+    public PostDTO updatePost( @PathVariable("postId") Long postId, @RequestBody PostDTO newPostDto){
+        Optional<Post> post = this.postRepository.findById(postId);
+        if(!post.isPresent()){
+            throw new NoSuchElementException("UserId inexistant");
+        }else if( post.get().getId() != newPostDto.getIdPost()){
+            throw new IllegalArgumentException();
+        }
+        else{
+            return  postConverter.entityToDto(postRepository.save(this.postConverter.dtoToEntity(newPostDto)));
+        }
+    }
+
+    /**
+     *
+     * @param postId
+     */
+    @DeleteMapping(path = "/{postId}")
+    public void deletePostById(@PathVariable ("postId") Long postId){
+        this.postRepository.deleteById(postId);
+    }
+
+    /**
+     *
+     */
+    @DeleteMapping(path = "")
+    public void deleteAllPost(){ this.postRepository.deleteAll();}
+
+    /**
+     * Should be optimized using deleteByIdIn from repository
+     * @param listIds
+     */
+    @DeleteMapping(path = "/list/{listIds}")
+    public void deletePostsByIds(@PathVariable("listIds") List<Long> listIds){
+        for (Long ids : listIds ) {
+            this.postRepository.deleteById(ids);
+        }
     }
 }
--- src/main/java/com/progparcomposant/reseausocial/repositories/PostRepository.java
+++ src/main/java/com/progparcomposant/reseausocial/repositories/PostRepository.java
@@ -2,6 +2,7 @@ package com.progparcomposant.reseausocial.repositories;
 
 import com.progparcomposant.reseausocial.model.Post;
 import com.progparcomposant.reseausocial.model.User;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -10,4 +11,5 @@ import java.util.Optional;
 public interface PostRepository extends CrudRepository<Post, Long> {
     Iterable<Post> findAllByUserId(Long userId);
     Iterable<Post> findAllByIdIn(List<Long> listIds);
+    void deleteByIdIn(List<Long> listIds);
 }
