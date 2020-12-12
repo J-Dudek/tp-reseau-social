e4d4abb8573a19336088f7d960901fa1e5c9ad1c
Julien Dudek <julien.dudek@gmail.com>
2020-12-12 01:44:08 +0100

    complément commit : oubli repo

--- src/main/java/com/progparcomposant/reseausocial/repositories/UserRepository.java
+++ src/main/java/com/progparcomposant/reseausocial/repositories/UserRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findUserByEmail(String email);
     List<User> findByIdIn(List<Long> ids);
+    Optional<User> findUserByUsername(String username);
 }
